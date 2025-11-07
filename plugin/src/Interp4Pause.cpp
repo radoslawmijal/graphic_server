ebd30bd98f79599362b7e604f339e110adfb870d
GoofyGarry420 <radekmijal@gmail.com>
2025-11-07 20:16:14 +0100

    Pause plugin added

---
+++ plugin/src/Interp4Pause.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include "Interp4Pause.hh"
+
+
+using std::cout;
+using std::endl;
+
+
+extern "C" {
+  AbstractInterp4Command* CreateCmd(void);
+  const char* GetCmdName() { return "Pause"; }
+}
+
+
+
+
+/*!
+ * \brief
+ *
+ *
+ */
+AbstractInterp4Command* CreateCmd(void)
+{
+  return Interp4Pause::CreateCmd();
+}
+
+
+/*!
+ *
+ */
+Interp4Pause::Interp4Pause(): _Time_ms(0)
+{}
+
+
+/*!
+ *
+ */
+void Interp4Pause::PrintCmd() const
+{
+  /*
+   *  Tu trzeba napisać odpowiednio zmodyfikować kod poniżej.
+   */
+  cout << GetCmdName() << " " << _Time_ms  << " time value" << endl;
+}
+
+
+/*!
+ *
+ */
+const char* Interp4Pause::GetCmdName() const
+{
+  return ::GetCmdName();
+}
+
+
+/*!
+ *
+ */
+bool Interp4Pause::ExecCmd( AbstractScene      &rScn, 
+                           const char         *sMobObjName,
+			   AbstractComChannel &rComChann
+			 )
+{
+  /*
+   *  Tu trzeba napisać odpowiedni kod.
+   */
+  return true;
+}
+
+
+/*!
+ *
+ */
+bool Interp4Pause::ReadParams(std::istream& Strm_CmdsList)
+{
+  /*
+   *  Tu trzeba napisać odpowiedni kod.
+   */
+  return true;
+}
+
+
+/*!
+ *
+ */
+AbstractInterp4Command* Interp4Pause::CreateCmd()
+{
+  return new Interp4Pause();
+}
+
+
+/*!
+ *
+ */
+void Interp4Pause::PrintSyntax() const
+{
+  cout << "   Pause  CzasPauzy[ms]" << endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,20 @@
 using namespace std;
 
 #define PLUGIN_NAME__move "libInterp4Move.so"
+#define PLUGIN_NAME__pause "libInterp4Pause.so"
 
 
 int main()
 {
   void *pLibHnd_Move = dlopen(PLUGIN_NAME__move,RTLD_LAZY);
   AbstractInterp4Command *(*pCreateCmd_Move)(void);
+
   void *pFun;
 
   if (!pLibHnd_Move) {
     cerr << "!!! Brak biblioteki: " PLUGIN_NAME__move << endl;
     return 1;
   }
-
 
   pFun = dlsym(pLibHnd_Move,"CreateCmd");
   if (!pFun) {
@@ -28,17 +29,50 @@ int main()
   pCreateCmd_Move = reinterpret_cast<AbstractInterp4Command* (*)(void)>(pFun);
 
 
-  AbstractInterp4Command *pCmd = pCreateCmd_Move();
+  AbstractInterp4Command *pCmd_Move = pCreateCmd_Move();
 
   cout << endl;
-  cout << pCmd->GetCmdName() << endl;
+  cout << pCmd_Move->GetCmdName() << endl;
   cout << endl;
-  pCmd->PrintSyntax();
+  pCmd_Move->PrintSyntax();
   cout << endl;
-  pCmd->PrintCmd();
+  pCmd_Move->PrintCmd();
   cout << endl;
   
-  delete pCmd;
+  delete pCmd_Move;
 
   dlclose(pLibHnd_Move);
+
+
+  /*    PAUSE   */
+
+  void *pLibHnd_Pause = dlopen(PLUGIN_NAME__pause,RTLD_LAZY);
+  AbstractInterp4Command *(*pCreateCmd_Pause)(void);
+
+  if (!pLibHnd_Pause) {
+    cerr << "!!! Brak biblioteki: " PLUGIN_NAME__pause << endl;
+    return 1;
+  }
+
+  pFun = dlsym(pLibHnd_Pause,"CreateCmd");
+  if (!pFun) {
+    cerr << "!!! Nie znaleziono funkcji CreateCmd" << endl;
+    return 1;
+  }
+  pCreateCmd_Pause = reinterpret_cast<AbstractInterp4Command* (*)(void)>(pFun);
+
+
+  AbstractInterp4Command *pCmd_Pause = pCreateCmd_Pause();
+
+  cout << endl;
+  cout << pCmd_Pause->GetCmdName() << endl;
+  cout << endl;
+  pCmd_Pause->PrintSyntax();
+  cout << endl;
+  pCmd_Pause->PrintCmd();
+  cout << endl;
+  
+  delete pCmd_Pause;
+
+  dlclose(pLibHnd_Pause);
 }
