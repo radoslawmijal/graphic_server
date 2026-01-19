b5d805dda40f1fc8415961063ae0113653f1348d
GoofyGarry420 <radekmijal@gmail.com>
2026-01-19 19:18:50 +0100

    Stage 4 files added

--- inc/ProgramInterpreter.hh
+++ inc/ProgramInterpreter.hh
@@ -3,13 +3,17 @@
 
 #include "Scene.hh"
 #include "Set4LibInterfaces.hh"
+#include "AbstractComChannel.hh"
 #include <iostream>
 #include <string>
+#include <mutex>
 
-class ProgramInterpreter {
+class ProgramInterpreter : public AbstractComChannel {
   private:
     Scene & _scn;
     Set4LibInterfaces & _libManager;
+    int _socket;
+    std::mutex _mtx;
 
   public:
     ProgramInterpreter(Scene & scn, Set4LibInterfaces & libManager) 
@@ -17,6 +21,11 @@ class ProgramInterpreter {
 
     bool ReadFile(const char* fileName);
     bool ExecProgram(const char* fileName);
+
+    bool ConnectToServer(const char* addr, int port);
+
+    virtual int GetSocket() const override { return _socket; }
+    virtual std::mutex & UseGuard() override { return _mtx; }
 };
 
 #endif
